5348f0f3157b02364943829d44c59958a7f889ec
Pieter Spruijt <pieter@pieterspruijt.nl>
2021-06-13 16:43:14 +0200

    added message event and ready event

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,9 +1,10 @@
 const config = require('../../config');
 
 module.exports = async (client) => {
+    console.log(`${client.user.tag} is ready!`);
   setInterval(() => {
     const index = Math.floor(Math.random() * (StatusList.length - 1) + 1);
-    c;ient.user.setActivity(StatusList[index]);
+    client.user.setActivity(StatusList[index]);
   }, 30000); // Runs this every 30 seconds.
   const StatusList = [
     ` in ${client.guilds.cache.size} guilds`,
